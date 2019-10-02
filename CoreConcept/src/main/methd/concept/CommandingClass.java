9b9b473063f3dda638d0f11398ac32fcae9ac811
pradeep <pradeepmaurya560@gmail.com>
2019-10-02 21:52:51 +0530

    Core java main method

---
+++ CoreConcept/src/main/methd/concept/CommandingClass.java
@@ -0,0 +1,17 @@
+package main.methd.concept;
+
+public class CommandingClass {
+
+    public static void main(String[] args) {
+   	System.out.println("Main Method");
+       }
+       
+       public static void main(String[] args,String name) {
+   	System.out.println("overloaded Main Method");
+       }
+
+       /*
+        * - No Compile Time ,No Run Time program will execute.
+        * - Execute actual main method.
+        */
+}

0eca0a44aaaf859131f8915666cab0c338dc59e4
pradeep <pradeepmaurya560@gmail.com>
2019-10-02 22:26:15 +0530

    println method is overloaded

---
+++ CoreConcept/src/string/concept/CommandingClass.java
@@ -0,0 +1,18 @@
+package string.concept;
+
+public class CommandingClass {
+
+    public static void main(String[] args) {
+	System.out.println("A"); //A
+	System.out.println('A');//A
+	System.out.println('A'+'A'); //130
+	
+	System.out.println(1); //1
+	System.out.println(1+1); //1
+	System.out.println(1+"1"); //11
+	
+	System.out.println('1'); //1
+	System.out.println('1'+'1'); //98
+    }
+
+}
